04b9064f09e0666cc4c6e3c767990752de8a1827
Kalyan <47467548+Ajith-Kalyan@users.noreply.github.com>
2022-06-16 22:46:47 +0100

    Best performing PPO model with code
    model saving and loading created.

--- lunarlander.py
+++ lunarlander.py
@@ -1,25 +1,46 @@
 import gym
 from stable_baselines3 import PPO
 
-env = gym.make("LunarLander-v2")
+import os
 
+
+MODEL_NAME = "PPO"
+models_dir = "models/{MODEL_NAME}"
+logdir = "logs"
+
+if not os.path.exists(models_dir):
+    	os.makedirs(models_dir)
+if not os.path.exists(logdir):
+    	os.makedirs(logdir)
+
+
+TIMESTEP = 10000
+MODEL_NAME = "PPO"
+
+env = gym.make("LunarLander-v2")
 env.reset()
 
-# print("sample action:", env.action_space.sample())
-# # print("observation space shape", env.observation_space.shape)
-# print("sample observation", env.observation_space.sample())
+#Loading a saved model
+models_dir = "models/{MODEL_NAME}"
+model_path = f"{models_dir}/PPO__2__490000.zip"
+model = MODEL_NAME.load(model_path, env = env)
 
+'''
 #Model creation
-model = PPO("MlpPolicy", env, verbose =1)
-model.learn(total_timesteps=70_000)
+model = MODEL_NAME("MlpPolicy", env, verbose =1, tensorboard_log=logdir)
+for i in range(1,50):
+	model.learn(total_timesteps=TIMESTEP, reset_num_timesteps=False,
+	 tb_log_name=MODEL_NAME)
+	model.save(f"{models_dir}/{MODEL_NAME}__2__{TIMESTEP*i}")
+'''
 
 episodes = 10
-
 for ep in range(episodes):
 		obs=env.reset()
 		done=False
 		while not done:
 			env.render()
-			obs, reward, done,info = env.step(env.action_space.sample())
+			action, _ = model.predict(obs) #model.predict also returns a state.
+			obs, reward, done,info = env.step(action)
 
 env.close()
